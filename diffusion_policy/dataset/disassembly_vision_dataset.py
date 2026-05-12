ba8e234a257edfe887b77c284e117c1222c2cf26
davoodso <davoodso@meen-mxl5073mbl.engr.tamu.edu>
2026-05-12 17:40:20 -0500

    Add disassembly vision diffusion policy baseline

---
+++ diffusion_policy/dataset/disassembly_vision_dataset.py
@@ -0,0 +1,129 @@
+from typing import Dict
+import copy
+import os
+
+import numpy as np
+import torch
+import zarr
+from threadpoolctl import threadpool_limits
+
+from diffusion_policy.common.normalize_util import get_image_range_normalizer
+from diffusion_policy.common.pytorch_util import dict_apply
+from diffusion_policy.common.replay_buffer import ReplayBuffer
+from diffusion_policy.common.sampler import SequenceSampler, downsample_mask, get_val_mask
+from diffusion_policy.dataset.base_dataset import BaseImageDataset
+from diffusion_policy.model.common.normalizer import LinearNormalizer, SingleFieldLinearNormalizer
+
+
+class DisassemblyVisionDataset(BaseImageDataset):
+    def __init__(
+        self,
+        shape_meta: dict,
+        dataset_path: str,
+        horizon=1,
+        pad_before=0,
+        pad_after=0,
+        n_obs_steps=None,
+        n_latency_steps=0,
+        load_to_memory=True,
+        seed=42,
+        val_ratio=0.05,
+        max_train_episodes=None,
+    ):
+        assert os.path.isdir(dataset_path), dataset_path
+
+        if load_to_memory:
+            replay_buffer = ReplayBuffer.copy_from_path(
+                dataset_path,
+                store=zarr.MemoryStore(),
+            )
+        else:
+            replay_buffer = ReplayBuffer.create_from_path(dataset_path, mode="r")
+
+        rgb_keys = list()
+        obs_shape_meta = shape_meta["obs"]
+        for key, attr in obs_shape_meta.items():
+            obs_type = attr.get("type", "low_dim")
+            if obs_type == "rgb":
+                rgb_keys.append(key)
+
+        key_first_k = dict()
+        if n_obs_steps is not None:
+            for key in rgb_keys:
+                key_first_k[key] = n_obs_steps
+
+        val_mask = get_val_mask(
+            n_episodes=replay_buffer.n_episodes,
+            val_ratio=val_ratio,
+            seed=seed,
+        )
+        train_mask = downsample_mask(
+            mask=~val_mask,
+            max_n=max_train_episodes,
+            seed=seed,
+        )
+
+        sampler = SequenceSampler(
+            replay_buffer=replay_buffer,
+            sequence_length=horizon + n_latency_steps,
+            pad_before=pad_before,
+            pad_after=pad_after,
+            episode_mask=train_mask,
+            key_first_k=key_first_k,
+        )
+
+        self.replay_buffer = replay_buffer
+        self.sampler = sampler
+        self.shape_meta = shape_meta
+        self.rgb_keys = rgb_keys
+        self.n_obs_steps = n_obs_steps
+        self.n_latency_steps = n_latency_steps
+        self.horizon = horizon
+        self.pad_before = pad_before
+        self.pad_after = pad_after
+        self.val_mask = val_mask
+
+    def get_validation_dataset(self):
+        val_set = copy.copy(self)
+        val_set.sampler = SequenceSampler(
+            replay_buffer=self.replay_buffer,
+            sequence_length=self.horizon + self.n_latency_steps,
+            pad_before=self.pad_before,
+            pad_after=self.pad_after,
+            episode_mask=self.val_mask,
+        )
+        return val_set
+
+    def get_normalizer(self, **kwargs) -> LinearNormalizer:
+        normalizer = LinearNormalizer()
+        normalizer["action"] = SingleFieldLinearNormalizer.create_fit(
+            self.replay_buffer["action"]
+        )
+        for key in self.rgb_keys:
+            normalizer[key] = get_image_range_normalizer()
+        return normalizer
+
+    def get_all_actions(self) -> torch.Tensor:
+        return torch.from_numpy(self.replay_buffer["action"][:])
+
+    def __len__(self):
+        return len(self.sampler)
+
+    def __getitem__(self, idx: int) -> Dict[str, torch.Tensor]:
+        threadpool_limits(1)
+        data = self.sampler.sample_sequence(idx)
+
+        obs_slice = slice(self.n_obs_steps)
+        obs_dict = dict()
+        for key in self.rgb_keys:
+            obs_dict[key] = np.moveaxis(data[key][obs_slice], -1, 1).astype(np.float32) / 255.0
+            del data[key]
+
+        action = data["action"].astype(np.float32)
+        if self.n_latency_steps > 0:
+            action = action[self.n_latency_steps :]
+
+        return {
+            "obs": dict_apply(obs_dict, torch.from_numpy),
+            "action": torch.from_numpy(action),
+        }
